50cb338af9fe5646a591d4b66f66d94c2441c184
kayzer24 <dante2410@hotmail.fr>
2020-03-31 16:17:48 +0200

    Updates

---
+++ precache-manifest.2d51f33dd8f0da0dcd3682b34596ba3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a1de49e46631e6721cd858240ef174",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "34d97a2c7d878981076b",
+    "url": "/monsters-rolodex/static/css/main.f2f09fd0.chunk.css"
+  },
+  {
+    "revision": "d5c2f8deb5713aa72604",
+    "url": "/monsters-rolodex/static/js/2.8d1a4dc1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.8d1a4dc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34d97a2c7d878981076b",
+    "url": "/monsters-rolodex/static/js/main.95cf1da1.chunk.js"
+  },
+  {
+    "revision": "70bb250c4736fa9b3c9c",
+    "url": "/monsters-rolodex/static/js/runtime-main.065f2892.js"
+  }
+]);
